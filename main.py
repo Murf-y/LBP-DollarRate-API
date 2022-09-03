dac7028e355fbd77cdf17fcb2485a18be3b38e61
Murf-y <charbelfayad64@gmail.com>
2022-09-03 15:08:26 +0300

    middleware

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from scrapper import get_dollar_rate
 
 
 app = FastAPI()
 baseUrl = '/api/v1'
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get(baseUrl + '/dollarRate')
 async def getRate() -> dict:
   buy_rate, sell_rate = await get_dollar_rate()
-  return {'buy_rate': buy_rate, 'sell_rate': sell_rate}
-
+  return {
+    'buy_rate': buy_rate,
+    'sell_rate': sell_rate
+  }
